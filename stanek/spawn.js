42fa1157d0569a5b4114b83135c8676e7c6aa42e
Jesse Clark <jjclark1982@gmail.com>
2022-03-12 11:11:20 -0800

    update stanek scripts

--- stanek/spawn.js
+++ stanek/spawn.js
@@ -2,17 +2,23 @@ const CHARGE = "/stanek/charge-x-y.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
-    const host = ns.args[0] || ns.getHostname();
-
-	const fragments = ns.stanek.activeFragments();
+    const fragments = ns.stanek.activeFragments();
     const xy = [];
     for (const fragment of fragments) {
         xy.push(fragment.x);
         xy.push(fragment.y);
     }
+
+    const host = ns.args[0] || ns.getHostname();
     const scriptRam = ns.getScriptRam(CHARGE, 'home');
     const server = ns.getServer(host);
-    const availableRam = server.maxRam - server.ramUsed; // - ns.getScriptRam(ns.getScriptName(), host);
+    let availableRam = server.maxRam - server.ramUsed;
+    if (host == ns.getHostname()) {
+        availableRam -= ns.getScriptRam(ns.getScriptName(), host);
+    }
+    if (host == 'home') {
+        availableRam -= 10.0;
+    }
     const threads = Math.floor(availableRam / scriptRam);
     await ns.scp(CHARGE, 'home', host);
     ns.exec(CHARGE, host, threads, ...xy);
