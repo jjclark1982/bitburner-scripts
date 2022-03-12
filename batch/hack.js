7408381e115b5860be75a3462f88887829fa0e05
Jesse Clark <jjclark1982@gmail.com>
2022-03-12 11:11:01 -0800

    add `--startTime` and `--verbose` flags to batch scripts

--- batch/hack.js
+++ batch/hack.js
@@ -1,10 +1,25 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-    const target = ns.args[0];
-    await ns.hack(target);
-    //ns.tprint("  Finished hack   "+JSON.stringify(ns.args));
-}
+const FLAGS = [
+    ["verbose", false],
+    ["startTime"]
+];
 
 export function autocomplete(data, args) {
+    data.flags(FLAGS);
     return data.servers;
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    const args = ns.flags(FLAGS);
+    const target = args._.shift();
+    if (args.startTime) {
+        await ns.sleep(args.startTime - Date.now());
+    }
+    if (args.verbose) {
+        ns.tprint(`  ${Date.now()}: Starting hack   ${JSON.stringify(ns.args)}`);
+    }
+    await ns.hack(target);
+    if (args.verbose) {
+        ns.tprint(`  ${Date.now()}: Finished hack   ${JSON.stringify(ns.args)}`);
+    }
 }
--- batch/weaken.js
+++ batch/weaken.js
@@ -1,10 +1,25 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-    const target = ns.args[0];
-    await ns.weaken(target);
-    //ns.tprint("Finished weaken "+JSON.stringify(ns.args));
-}
+const FLAGS = [
+    ["verbose", false],
+    ["startTime"]
+];
 
 export function autocomplete(data, args) {
+    data.flags(FLAGS);
     return data.servers;
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    const args = ns.flags(FLAGS);
+    const target = args._.shift();
+    if (args.startTime) {
+        await ns.sleep(args.startTime - Date.now());
+    }
+    if (args.verbose) {
+        ns.tprint(`  ${Date.now()}: Starting weaken ${JSON.stringify(ns.args)}`);
+    }
+    await ns.weaken(target);
+    if (args.verbose) {
+        ns.tprint(`  ${Date.now()}: Finished weaken ${JSON.stringify(ns.args)}`);
+    }
 }
