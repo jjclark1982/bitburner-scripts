939a8f2b74770b1f7e3c50686aaee9cab3c024ff
Jesse Clark <jjclark1982@gmail.com>
2022-03-16 09:54:55 -0700

    update `sleeves/buy-augs`

--- sleeves/buy-augs.js
+++ sleeves/buy-augs.js
@@ -1,3 +1,7 @@
+const EXCLUDED_AUGS = {
+    "CashRoot Starter Kit": true
+};
+
 export async function main(ns) {
     let fundsFraction = 0.01;
     if (ns.args.length > 0) {
@@ -5,6 +9,9 @@ export async function main(ns) {
     }
     for (let i = 0; i < ns.sleeve.getNumSleeves(); i++) {
         for (const aug of ns.sleeve.getSleevePurchasableAugs(i)) {
+            if (aug.name in EXCLUDED_AUGS) {
+                continue;
+            }
             if (aug.cost < fundsFraction*ns.getPlayer().money) {
                 if (ns.sleeve.purchaseSleeveAug(i, aug.name)) {
                     ns.tprint(`Sleeve ${i} purchased ${aug.name}`);
