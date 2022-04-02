a73e9584451e7a4f78c936c4814800d34af51cf9
Jesse Clark <jjclark1982@gmail.com>
2022-04-02 15:08:56 -0700

    update script path

--- hacking/spawn-unmanaged-hacking.js
+++ hacking/spawn-unmanaged-hacking.js
@@ -17,7 +17,7 @@ export async function main(ns) {
     const money = ns.getServerMoneyAvailable(target);
     const threads = Math.ceil(ns.growthAnalyze(target, maxMoney / Math.max(1,money)) / 2);
 
-    const script = "/batch/early-hacking.js";
+    const script = "/hacking/unmanaged-hacking.js";
     const args = [target, "--minSecurity", minSecurity, "--maxMoney", maxMoney];
 
     // ns.run(script, threads, ...args);
