f24e7aa1f04a16a5f47aff5bdb21d88d6edfa032
Jesse Clark <jjclark1982@gmail.com>
2022-04-02 12:03:05 -0700

    dodge the corporation API RAM cost

--- corporation/buyback-shares.js
+++ corporation/buyback-shares.js
@@ -1,13 +1,14 @@
 export async function main(ns) {
     const player = ns.getPlayer();
     if (player.hasCorporation) {
-        const corp = ns.corporation.getCorporation();
+        const corpAPI = eval("ns.corporation");
+        const corp = corpAPI.getCorporation();
         if (corp.public) {            
             if (corp.issuedShares > 0) {
                 const buybackPrice = corp.sharePrice * 1.1;
                 const buybackMoney = Math.min(player.money, corp.issuedShares * buybackPrice);
                 const numShares = buybackMoney / buybackPrice;
-                ns.corporation.buyBackShares(numShares);
+                corpAPI.buyBackShares(numShares);
             }
         }
     }
