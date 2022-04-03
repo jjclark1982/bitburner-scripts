1e60ef92df46ec9fa30c725de987185d677ee2c9
Jesse Clark <jjclark1982@gmail.com>
2022-04-02 22:36:17 -0700

    optional focus for training script

--- player/train.js
+++ player/train.js
@@ -1,10 +1,17 @@
+/*
+
+run player/train.js --strength 100 --defense 100 --dexterity 100 --agility 100 --focus
+
+*/
+
 const FLAGS = [
     ['hacking', 0],
     ['charisma', 0],
     ['strength', 0],
     ['defense', 0],
     ['dexterity', 0],
-    ['agility', 0]
+    ['agility', 0],
+    ['focus', false]
 ];
 
 export function autocomplete(data, args) {
@@ -16,15 +23,17 @@ export function autocomplete(data, args) {
 export async function main(ns) {
     ns.disableLog("sleep");
     const flags = ns.flags(FLAGS);
+    const focus = flags.focus;
+    delete flags.focus;
     delete flags._;
-    await prepareStats(ns, flags);
+    await prepareStats(ns, flags, focus);
 }
 
-export async function prepareStats(ns, targetStats) {
+export async function prepareStats(ns, targetStats, focus=false) {
     for (const [stat, statTarget] of Object.entries(targetStats)) {
         while (ns.getPlayer()[stat] < statTarget) {
             if (!ns.isBusy()) {
-                trainStat(ns, stat);
+                trainStat(ns, stat, focus);
             }
             await ns.sleep(1*1000);
         }
@@ -32,14 +41,14 @@ export async function prepareStats(ns, targetStats) {
     }
 }
 
-export function trainStat(ns, stat) {
+export function trainStat(ns, stat, focus=false) {
     if (stat == 'hacking') {
-        ns.universityCourse("Rothman University", "Algorithms");
+        ns.universityCourse("Rothman University", "Algorithms", focus);
     }
     else if (stat == 'charisma') {
-        ns.universityCourse("Rothman University", "Leadership");
+        ns.universityCourse("Rothman University", "Leadership", focus);
     }
     else {
-        ns.gymWorkout("Powerhouse Gym", stat);
+        ns.gymWorkout("Powerhouse Gym", stat, focus);
     }
 }
