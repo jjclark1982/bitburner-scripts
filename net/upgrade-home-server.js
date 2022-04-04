fceec759cf2febb0ce497a95dba13b975ad01ced
Jesse Clark <jjclark1982@gmail.com>
2022-04-04 09:26:19 -0700

    fix typo

--- net/upgrade-home-server.js
+++ net/upgrade-home-server.js
@@ -3,13 +3,13 @@ export async function main(ns) {
 }
 
 export async function upgradeHomeComputer(ns, moneyFraction=0.75) {
-    while (ns.getPlayer().money * moneyfraction > ns.getUpgradeHomeRamCost()) {
+    while (ns.getPlayer().money * moneyFraction > ns.getUpgradeHomeRamCost()) {
         const cost = ns.nFormat(ns.getUpgradeHomeRamCost(), "$0.0 a");
         ns.upgradeHomeRam();
         ns.tprint(`Upgraded home computer RAM for ${cost}`);
         await ns.sleep(100);
     }
-    while (ns.getPlayer().money * moneyfraction > ns.getUpgradeHomeCoresCost()) {
+    while (ns.getPlayer().money * moneyFraction > ns.getUpgradeHomeCoresCost()) {
         const cost = ns.nFormat(ns.getUpgradeHomeCoresCost(), "$0.0 a");
         ns.upgradeHomeCores();
         ns.tprint(`Upgraded home computer cores for ${cost}`);
