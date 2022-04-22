9fa4c3020f64014cfaed9587c4163d25a4212fc7
Jesse Clark <jjclark1982@gmail.com>
2022-04-21 18:01:52 -0700

    fall back to computing forecast from history when 4s tix api is not available

--- stocks/sell.js
+++ stocks/sell.js
@@ -1,6 +1,6 @@
-import {getAllStocks, sellStocks} from "stocks/naive-trader.js"
+import { StockTrader } from "stocks/trader.js"
 
 export async function main(ns) {
-    const allStocks = getAllStocks(ns);
-    sellStocks(ns, Object.values(allStocks));
+    const trader = new StockTrader(ns);
+    trader.sellStocks(trader.getAllStocks());
 }
--- stocks/trader.js
+++ stocks/trader.js
@@ -1,176 +1,279 @@
-import { StockSymbols as CompanyStockSymbols } from '/stocks/companies';
+import { drawTable } from "/lib/box-drawing";
 import { PortService } from "/lib/port-service";
+import { StockSymbols as CompanyStockSymbols } from "/stocks/companies";
 
 export async function main(ns) {
     ns.disableLog("asleep");
     ns.disableLog("stock.buy");
     ns.disableLog("stock.sell");
 
-    const stockInfo = new StockInfo(ns);
+    const stockInfo = new StockTrader(ns);
     const service = new PortService(ns, 5, stockInfo);
     await service.serve(stockInfo);
 }
 
-class StockInfo {
+class StockTrader {
     constructor(ns) {
         this.ns = ns;
         this.lastUpdateTime = 1;
+        this.priceHistory = {};
+        for (const symbol of ns.stock.getSymbols()) {
+            this.priceHistory[symbol] = [];
+        }
     }
 
     getStockInfo(symbol) {
-        return getStockInfo(this.ns, symbol);
+        const {ns} = this;
+        // look up organization name
+        if (symbol in CompanyStockSymbols) {
+            symbol = CompanyStockSymbols[symbol];
+        }
+        if (!ns.stock.getSymbols().includes(symbol)) {
+            return null;
+        }
+        const pos = ns.stock.getPosition(symbol);
+        const stock = {
+            symbol: symbol,
+            forecast: this.computeForecast(symbol),
+            volatility: this.computeVolatility(symbol),
+            askPrice: ns.stock.getAskPrice(symbol),
+            bidPrice: ns.stock.getBidPrice(symbol),
+            maxShares: ns.stock.getMaxShares(symbol),
+            shares: pos[0],
+            sharesAvgPrice: pos[1],
+            sharesShort: pos[2],
+            sharesAvgPriceShort: pos[3],
+        };
+        stock.price = (stock.askPrice + stock.bidPrice) / 2;
+        stock.netShares = stock.shares - stock.sharesShort;
+        stock.netValue = stock.bidPrice * stock.shares - stock.askPrice * stock.sharesShort;
+        stock.summary = `${stock.symbol} (${(stock.forecast*100).toFixed(1)}% ± ${(stock.volatility*100).toFixed(1)}%)`;
+        return stock;
+    }
+    
+    [Symbol.iterator]() {
+        return this.getAllStocks()[Symbol.iterator]();
     }
 
     getAllStocks() {
-        return getAllStocks(this.ns);
+        const {ns} = this;
+        return ns.stock.getSymbols().map((symbol)=>this.getStockInfo(symbol));
     }
 
     getPortfolioValue() {
-        return getPortfolioValue(this.getAllStocks());
+        return [...this].reduce((total, stock)=>(
+            total + stock.netValue
+        ), 0);
+    }
+
+    /**
+     * Select stocks with at least threshold % chance to increase each tick
+     * @param {number} threshold 
+     * @returns {Stock[]}
+     */
+    getBullStocks(threshold=0.55) {
+        return [...this].filter((stock)=>(
+            stock.forecast - stock.volatility > threshold
+        ));
+    }
+
+    /**
+     * Select stocks with at most threshold % chance to increase each tick
+     * @param {number} threshold 
+     * @returns {Stock[]}
+     */
+     getBearStocks(stocks, threshold=0.48) {
+        return [...this].filter((stock)=>(
+            stock.forecast - stock.volatility < threshold
+        ));
+    }
+
+    computeForecast(symbol) {
+        if (ns.getPlayer().has4SDataTixApi) {
+            return this.ns.stock.getForecast(symbol);
+        }
+        const history = this.priceHistory(symbol);
+        if (history.length < 2) {
+            return 0.5;
+        }
+        let increases = 0;
+        let decreases = 0;
+        for (let i = 1; i < history.length; i++) {
+            const change = (history[i] - history[i-1]) / history[i];
+            if (change > 0) {
+                increases += 1;
+            }
+            else {
+                decreases += 1;
+            }
+        }
+        const chanceToIncrease = increases / (increases + decreases);
+        return chanceToIncrease;
+        //const ratios = [];
+        //for (let i = 1; i < history.length; i++) {
+        //    const ratio = history[i] / history[i-1];
+        //    ratios.push(ratio);
+        //}
+        //const avgRatio = ratios.reduce((a, b) => a + b, 0) / ratios.length;
+        //return avgRatio / 2;
+    }
+
+    computeVolatility(symbol) {
+        if (ns.getPlayer().has4SDataTixApi) {
+            return this.ns.stock.getVolatility(symbol);
+        }
+        const history = this.priceHistory(symbol);
+        if (history.length < 2) {
+            return 0;
+        }
+        const changes = [];
+        for (let i = 1; i < history.length; i++) {
+            const change = (history[i] - history[i-1]) / history[i];
+            changes.push(Math.abs(change));
+        }
+        const maxChange = Math.max(...changes);
+        return maxChange;
+    }
+
+    updatePriceHistory() {
+        const {ns} = this;
+        for (const symbol of ns.stock.getSymbols()) {
+            const stock = this.getStockInfo(symbol);
+            const price = (stock.askPrice + stock.bidPrice) / 2;
+
+            this.priceHistory[symbol].push(price);
+    
+            // only track the last 16-32 events
+            if (this.priceHistory[symbol].length > 32) {
+                this.priceHistory[symbol] = this.priceHistory[symbol].slice(16);
+            }
+        }
     }
 
     update() {
-        // run at most once per minute
+        // Run at most once per minute.
+        // TODO: match this to actual stock tick time. Does it have readable bonusTime like other mechanics?
         const now = Date.now();
         if (now < this.lastUpdateTime + 1*60*1000) {
             return;
         }
         this.lastUpdateTime = now;
 
-        tendStocks(this.ns);
+        this.updatePriceHistory();
+        this.tendStocks();
     }
-}
 
-function tendStocks(ns) {
-    const allStocks = getAllStocks(ns);
-    
-    // select stocks with <51% chance to increase price
-    const stocksToSell = getBearStocks(allStocks, 0.51);
-    // sell all those stocks
-    sellStocks(ns, stocksToSell);
-    
-    // select stocks with >55% chance to increase price
-    const stocksToBuy = getBullStocks(allStocks, 0.55);
-    // buy the highest-rated stocks available
-    buyStocks(ns, stocksToBuy);
+    tendStocks(ns) {
+        // select stocks with <51% chance to increase price
+        const stocksToSell = this.getBearStocks(0.51);
+        // sell all those stocks
+        this.sellStocks(stocksToSell);
+        
+        // select stocks with >55% chance to increase price
+        const stocksToBuy = this.getBullStocks(0.55);
+        // buy the highest-rated stocks available
+        this.buyStocks(stocksToBuy);
+    }    
 
-    // keep of log of net worth change over time
-    const portfolioValue = getPortfolioValue(allStocks);
-    const cashValue = ns.getPlayer().money;
-    const totalValue = portfolioValue + cashValue;
-    ns.print(`Net worth: ${ns.nFormat(totalValue, "$0.000a")} = ${ns.nFormat(portfolioValue, "$0.0a")} stocks + ${ns.nFormat(cashValue, "$0.0a")} cash`);
-}
-
-export function getStockInfo(ns, symbol) {
-    getStockInfo.allSymbols ||= ns.stock.getSymbols();
-    if (symbol in CompanyStockSymbols) {
-        // look up organization name
-        symbol = CompanyStockSymbols[symbol];
-    }
-    if (!getStockInfo.allSymbols.includes(symbol)) {
-        return null;
-    }
-    const pos = ns.stock.getPosition(symbol);
-    const stock = {
-        symbol: symbol,
-        forecast: ns.stock.getForecast(symbol),
-        volatility: ns.stock.getVolatility(symbol),
-        askPrice: ns.stock.getAskPrice(symbol),
-        bidPrice: ns.stock.getBidPrice(symbol),
-        maxShares: ns.stock.getMaxShares(symbol),
-        shares: pos[0],
-        sharesAvgPrice: pos[1],
-        sharesShort: pos[2],
-        sharesAvgPriceShort: pos[3],
-    };
-    stock.netShares = stock.shares - stock.sharesShort;
-    stock.netValue = stock.bidPrice * stock.shares - stock.askPrice * stock.sharesShort;
-    stock.summary = `${stock.symbol}: ${stock.forecast.toFixed(3)} ± ${stock.volatility.toFixed(3)}`;
-    return stock;
-}
-
-export function getAllStocks(ns) {
-    // make a lookup table of all stocks and all their properties
-    const stockSymbols = ns.stock.getSymbols();
-    const stocks = {};
-    for (const symbol of stockSymbols) {
-        stocks[symbol] = getStockInfo(ns, symbol);
-    }
-    return stocks;
-}
-
-function getPortfolioValue(stocks) {
-    let value = 0;
-    for (const stock of Object.values(stocks)) {
-        value += stock.netValue;
-    }
-    return value;
-}
-
-function getBullStocks(stocks, threshold=0.55) {
-    // select stocks with at least threshold % chance to increase each tick
-    const bullStocks = [];
-    for (const stock of Object.values(stocks)) {
-        if (stock.forecast - stock.volatility > threshold) {
-            bullStocks.push(stock);
-        }
-    }
-    return bullStocks;
-}
-
-function getBearStocks(stocks, threshold=0.48) {
-    // select stocks with at most threshold % chance to increase each tick
-    const bearStocks = [];
-    for (const stock of Object.values(stocks)) {
-        if (stock.forecast - stock.volatility < threshold) {
-            bearStocks.push(stock);
-        }
-    }
-    return bearStocks;
-}
-
-
-export function sellStocks(ns, stocksToSell) {
-    for (const stock of stocksToSell) {
-        if (stock.shares > 0) {
-            const salePrice = ns.stock.sell(stock.symbol, stock.shares);
-            if (salePrice != 0) {
-                const saleTotal = salePrice * stock.shares;
-                const saleCost = stock.sharesAvgPrice * stock.shares;
-                const saleProfit = saleTotal - saleCost;
-                stock.shares = 0;
-                //ns.print(`Sold ${ns.nFormat(saleTotal, "$0.0a")} of ${stock.summary}`);
-                ns.print(`Sold ${stock.summary} stock for ${ns.nFormat(saleProfit, "$0.0a")} profit`);
+    /**
+     * Sell all shares of the given stocks.
+     * @param {Stock[]} stocksToSell - lowest rated stocks
+     */
+    sellStocks(stocksToSell) {
+        const {ns} = this;
+        for (const stock of stocksToSell) {
+            if (stock.shares > 0) {
+                const salePrice = ns.stock.sell(stock.symbol, stock.shares);
+                if (salePrice != 0) {
+                    const saleTotal = salePrice * stock.shares;
+                    const saleCost = stock.sharesAvgPrice * stock.shares;
+                    const saleProfit = saleTotal - saleCost;
+                    stock.shares = 0;
+                    //ns.print(`Sold ${ns.nFormat(saleTotal, "$0.0a")} of ${stock.summary}`);
+                    ns.print(`Sold ${stock.summary} stock for ${ns.nFormat(saleProfit, "$0.0a")} profit`);
+                }
             }
         }
     }
-}
-
-function buyStocks(ns, stocksToBuy, moneyToSpend, maxTransactions=4) {
-    // buy stocks, spending more money on higher rated stocks
-    const bestStocks = stocksToBuy.sort((a,b)=>{
-        return b.forecast - a.forecast; // descending
-    });
-
-    let transactions = 0;
-    for (const stock of bestStocks) {
-        const moneyRemaining = ns.getPlayer().money;
-        // don't spend the last 5 million bux
-        if (moneyRemaining < 5000000 || transactions >= maxTransactions) {
-            return;
+    
+    /**
+     * Buy stocks, spending more money on higher rated stocks
+     * @param {Stock[]} stocksToBuy - highest rated stocks
+     * @param {number} [maxTransactions=4]
+     * @returns 
+     */
+    buyStocks(stocksToBuy, maxTransactions=4) {
+        const {ns} = this;
+        const bestStocks = stocksToBuy.sort((a,b)=>{
+            return b.forecast - a.forecast; // descending
+        });
+    
+        let transactions = 0;
+        for (const stock of bestStocks) {
+            const moneyRemaining = ns.getPlayer().money;
+            // don't spend the last 5 million bux
+            if (moneyRemaining < 5000000 || transactions >= maxTransactions) {
+                return;
+            }
+            // spend up to half the money available on the highest rated stock
+            // (the next stock will buy half as much)
+            const moneyThisStock = moneyRemaining/2 - 100000;
+            let numShares = moneyThisStock / stock.askPrice;
+            
+            numShares = Math.min(numShares, stock.maxShares - stock.shares - stock.sharesShort);
+            const boughtPrice = ns.stock.buy(stock.symbol, numShares);
+            if (boughtPrice != 0) {
+                const boughtTotal = boughtPrice * numShares;
+                transactions += 1;
+                stock.shares += numShares;
+                ns.print(`Bought ${ns.nFormat(boughtTotal, "$0.0a")} of ${stock.summary}`);
+            }
         }
-        // spend up to half the money available on the highest rated stock
-        // (the next stock will buy half as much)
-        const moneyThisStock = moneyRemaining/2 - 100000;
-        let numShares = moneyThisStock / stock.askPrice;
-        
-        numShares = Math.min(numShares, stock.maxShares - stock.shares - stock.sharesShort);
-        const boughtPrice = ns.stock.buy(stock.symbol, numShares);
-        if (boughtPrice != 0) {
-            const boughtTotal = boughtPrice * numShares;
-            transactions += 1;
-            stock.shares += numShares;
-            ns.print(`Bought ${ns.nFormat(boughtTotal, "$0.0a")} of ${stock.summary}`);
+    }
+
+    report() {
+        const {ns} = this;
+
+        const portfolioValue = this.getPortfolioValue();
+        const cashValue = ns.getPlayer().money;
+        const totalValue = portfolioValue + cashValue;
+
+        function formatPct(n) {
+            if (!n) { return ''; }
+            return `${(n*100).toFixed(1)}%`;
         }
+        function formatPrice(n) {
+            if (!n) { return ''; }
+            return ns.nFormat(n, "$0,0.0a");
+        }
+
+        const columns1 = [
+            {header: 'Stock', field: 'symbol', align:'left'},
+            {header: 'Forecast', field: 'forecast', format: formatPct},
+            {header: 'Volatil.', field: 'volatility', format: formatPct},
+            {header: 'Price', field: 'price', width: 7, align:'right', format:formatPrice},
+            {header: 'Held', field: 'netValue', width: 7, align:'right', format:formatPrice},
+        ];
+        columns1.title = "Stock Portfolio";
+        const stockRows = [...this].filter((stock)=>(
+            stock.netValue != 0
+        )).sort((a,b)=>(
+            b.netValue - a.netValue
+        ));
+        const portfolioRow = {symbol: "Total", price: 0, netValue: portfolioValue};
+
+        const table1 = drawTable(columns1, stockRows, [portfolioRow]);
+
+        const columns2 = [
+            {header: 'Cash', field: 'label', width:27, align:'left'},
+            {header: formatPrice(cashValue), field: 'value', align:'right', width:17, format:formatPrice}
+        ];
+
+        const cashRow = {label: "Cash", value: cashValue};
+        const netWorthRow = {label: "Net Worth", value: totalValue};
+
+        const table2 = drawTable(columns2, [netWorthRow])
+
+        return (table1 + '\n' + table2);
     }
 }
