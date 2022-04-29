924ddabdbf25da78d2f596244cdd666deb7c1252
Jesse Clark <jjclark1982@gmail.com>
2022-04-29 14:49:03 -0700

    remove dependencies from `stocks/sell.js`

--- stocks/sell.js
+++ stocks/sell.js
@@ -1,6 +1,17 @@
-import { StockTrader } from "/stocks/trader";
-
 export async function main(ns) {
-    const trader = new StockTrader(ns);
-    trader.sellStocks(trader.getAllStocks());
+    sellAllStocks(ns);
+}
+
+export function sellAllStocks(ns, short=false) {
+    let netMoney = 0;
+    for (const symbol of ns.stock.getSymbols()) {
+        const [shares, avgPrice, sharesShort, avgPriceShort] = ns.stock.getPosition(symbol);
+        const salePrice = ns.stock.sell(symbol, shares);
+        netMoney += shares * salePrice;
+        if (short) {
+            const saleCost = ns.stock.sellShort(symbol, sharesShort);
+            netMoney -= sharesShort * saleCost;
+        }
+    }
+    ns.tprint(`INFO: Sold all stocks for ${ns.nFormat(netMoney, "$0.0a")}`);
 }
