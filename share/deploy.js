78a99394206e767efb2773db0d2d5a723ce40d45
Jesse Clark <jjclark1982@gmail.com>
2022-05-05 13:13:15 -0700

    print pid of deployed scripts

--- share/deploy.js
+++ share/deploy.js
@@ -15,5 +15,5 @@ export async function main(ns) {
     const reservedRam = ns.args[1];
     const script = "/share/share.js";
 
-    await deploy(ns, {host, script, reservedRam, threads:'max'});
+    await deploy(ns, {host, script, reservedRam, threads:'max'}, {logLevel: 4});
 }
--- stanek/deploy.js
+++ stanek/deploy.js
@@ -24,5 +24,5 @@ export async function main(ns) {
     const script = "/stanek/charge-x-y.js";
     const args = xy;
 
-    await deploy(ns, {host, script, args, reservedRam, threads:'max'});
+    await deploy(ns, {host, script, args, reservedRam, threads:'max'}, {logLevel: 4});
 }
